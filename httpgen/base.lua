eab84562c6033c37665fc25833e1caa52028031a
amipy <46629183+amipy@users.noreply.github.com>
2023-03-07 09:17:55 +0100

    Split module and message type in http base function

--- httpgen/base.lua
+++ httpgen/base.lua
@@ -1,6 +1,5 @@
 function mschttp.NAME(msgtype, data)
-    msgtype='PREFIX'+msgtyp
-    data={['type']=msgtype,['data']=data}
+    data={['module']=PREFIX,['type']=msgtype,['data']=data}
     json=to_json(data)
     for k, v in pairs(uri_reserved) do
         if string.find(json, k, 0, true) then
