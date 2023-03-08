2eae73ebdaaa5ff4470a4a8cac40b19ace7089b0
amipy <alex.ilves@outlook.com>
2023-03-08 11:16:50 +0100

    Added (blank) update_bin.sh and (likely broken) serverutils.py unix support

--- serverutils.py
+++ serverutils.py
@@ -1,7 +1,14 @@
 import os
+import platform
+
+def _get_os():
+    return platform.platform()
 
 def update(server):
-    os.system(rf'utils\update_bin.bat {server}')
+    if _get_os()=="Windows":
+        os.system(rf'utils\update_bin.bat {server}')
+    else:
+        os.system(f'./utils/update_bin.sh {server}')
 
 def makedir(server):
     if not os.path.exists(f'servers/{server}/'):
@@ -12,4 +19,7 @@ def makedir(server):
         os.mkdir(f'servers/{server}/conf')
 
 def runserver(server):
-    os.system(rf'servers\{server}\bin\server64.exe +server_dir \servers\{server}\conf\\')
+    if _get_os() == "Windows":
+        os.system(rf'servers\{server}\bin\server64.exe +server_dir \servers\{server}\conf\\')
+    else:
+        os.system(rf'./servers/{server}/bin/server64.exe +server_dir /servers/{server}/conf/')
