18ec9fb5f1e47350353b5a5284194ed224dffa16
amipy <alex.ilves@outlook.com>
2023-03-08 10:15:27 +0100

    Added steamcmd for server updates

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
 from pathlib import Path
 
 import generator
-generator.generate(Path("test"), extract=False, http_port=1000)
+#generator.generate(Path("test"), extract=False, http_port=1000)
+os.system(r'utils\update_bin.bat msc_test')
