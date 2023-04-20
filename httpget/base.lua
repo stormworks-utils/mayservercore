96584cbe1213677f0ac57c262c3b48c9a1e4a133
Fabian Wunsch <fabian@uriah.heep.sax.de>
2023-04-20 08:02:27 +0200

    simpler escaping

--- httpget/base.lua
+++ httpget/base.lua
@@ -1,10 +1,5 @@
 function mschttp.NAME(msgtype, data)
     data={['module']='PREFIX',['type']=msgtype,['data']=data}
     json=to_json(data)
-    for k, v in pairs(uri_reserved) do
-        if string.find(json, k, 0, true) then
-            json = string.gsub(json, escape(k), v)
-        end
-    end
-    server.httpGet(PORT, "/mschttp?data=" .. json)
+    server.httpGet(PORT, "/mschttp?data=" .. escape(json))
 end
