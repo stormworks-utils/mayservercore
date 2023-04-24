dbe7455e0a6e7c3af6548006d6b6620c1a224aeb
Fabian Wunsch <fabian@uriah.heep.sax.de>
2023-04-24 22:03:03 +0200

    Refactored handlers.py

--- handlers.py
+++ handlers.py
@@ -1,34 +1,33 @@
 from pathlib import Path
-def generate_handler(dir, static, dynamic, repl_head=False, repl_end=False):
-    with open(Path(dir) / 'base') as base_h:
+from typing import Any
+
+
+def replace_placeholders(base_string: str, replacement_dict: dict[str, Any]) -> str:
+    for name, value in replacement_dict.items():
+        base_string.replace(name, str(value))
+    return base_string
+
+
+def generate_handler(
+        handler_directory: str,
+        static: dict[str, Any],
+        dynamic: list[dict[str, Any]],
+        repl_head: bool = False,
+        repl_end: bool = False
+) -> str:
+    with open(Path(handler_directory) / 'base') as base_h:
         base_function = base_h.read()
-    with open(Path(dir) / 'head') as head_h:
+    with open(Path(handler_directory) / 'head') as head_h:
         head_function = head_h.read()
-    with open(Path(dir) / 'end') as end_h:
+    with open(Path(handler_directory) / 'end') as end_h:
         end_function = end_h.read()
-    string = ''
     if repl_head:
-        working = head_function
-        for placeholder in static.keys():
-            value = static[placeholder]
-            working = working.replace(str(placeholder), str(value))
-        string+=working+'\n'
-    else:
-        string+=head_function+'\n'
+        head_function = replace_placeholders(head_function, static)
     if repl_end:
-        working = end_function
-        for placeholder in static.keys():
-            value = static[placeholder]
-            working = working.replace(str(placeholder), str(value))
-        end_function=working
+        end_function = replace_placeholders(end_function, static)
+    string: str = head_function + '\n'
+    base_function = replace_placeholders(base_function, static)
     for dyn in dynamic:
-        working = base_function
-        for placeholder in static.keys():
-            value = static[placeholder]
-            working = working.replace(str(placeholder), str(value))
-        for placeholder in dyn.keys():
-            value = dyn[placeholder]
-            working = working.replace(str(placeholder), str(value))
-        string+=working+'\n'
-    string+=end_function+'\n'
+        string += replace_placeholders(base_function, dyn) + '\n'
+    string += end_function + '\n'
     return string
