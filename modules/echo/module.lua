1add4a811822a2a3604e9e46239a9989b5643444
amipy <alex.ilves@outlook.com>
2023-08-20 23:43:48 +0200

    Updated discord chat relay to use python configs

--- modules/echo/module.lua
+++ modules/echo/module.lua
@@ -1,8 +1,5 @@
-webhook='###CONFIG:webhook:none'
 function onChatMessage(peerID, name, message)
-    if webhook~='none' then
-        server.httpGet('chat',{name=name,message=message,webhook=webhook})
-    end
+    server.httpGet('chat',{name=name,message=message})
 end
 
 function httpReply()
--- modules/echo/module.py
+++ modules/echo/module.py
@@ -1,11 +1,17 @@
 import requests
+import json
+
+with open('discord_chat_relay.json') as config:
+    conf=json.load(config)
+
+webhook=conf.get("webhook") or None
 def handler(datatype, message):
     if datatype=='chat':
-        target=message['webhook']
-        name=message['name']
-        content=message['message']
+        if webhook:
+            name=message['name']
+            content=message['message']
 
-        data = {"username": name, "content": content}
-        requests.post(target, data)
+            data = {"username": name, "content": content}
+            requests.post(webhook, data)
 
     return {}
